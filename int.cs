1858a017831e0c6489312f2d272b877c8e067568
onurakyildiz58 <56562773+onurakyildiz58@users.noreply.github.com>
2019-10-15 00:20:34 +0300

    onur akyıldız
    bunlar benim deneme çalışmalarımdır.

---
+++ int.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace int_değişkeni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5674;
+            int b = 8736;
+
+            int toplam = a + b;
+
+            Console.WriteLine(toplam);
+
+            Console.Read();
+        }
+    }
+}
---
+++ kare ve dikdörtgen alan çevre hesalama.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pouıb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kenar, karealan, karecevre;
+
+            Console.WriteLine("**KARE ÇEVRE VE ALAN HESAPLAMA**");
+          
+            Console.Write("kenar giriniz= ");
+            kenar = Convert.ToInt32(Console.ReadLine());
+
+            karecevre = kenar * 4;
+            karealan = kenar * kenar;
+
+            Console.WriteLine("karecevre=" + karecevre);
+            Console.WriteLine("karealan=" + karealan);
+
+            Console.Read();
+
+            int kenar1, kenar2, dikdortgenalan, dikdortgencevre;
+           
+            Console.WriteLine("**DİKDÖRTGEN ALAN VE ÇEVRE HESAPLAMA**");
+
+            Console.Write("kenar1 giriniz= ");
+            kenar1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("kenar2 giriniz= ");
+            kenar2 = Convert.ToInt32(Console.ReadLine());
+
+            dikdortgenalan = kenar1 * kenar2;
+            dikdortgencevre = (kenar1 + kenar2) * 2;
+
+            Console.WriteLine("dikdortgencevre=" + dikdortgencevre);
+            Console.WriteLine("dikdortgenalan=" + dikdortgenalan);
+
+            Console.Read();
+
+
+        }
+    }
+}
