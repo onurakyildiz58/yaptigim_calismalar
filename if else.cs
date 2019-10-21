940b77cde797370b686ef11450bd29dcbbb641b5
onurakyildiz58 <56562773+onurakyildiz58@users.noreply.github.com>
2019-10-21 22:27:04 +0300

    Add files via upload

---
+++ if else.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string isim;
+            Console.WriteLine("isminizi giriniz= ");
+            isim = Console.ReadLine();
+
+            if (isim=="onur akyıldız")
+                {
+                Console.WriteLine("doğru isim");
+                }
+                else
+                {
+                Console.WriteLine("yanlış isim");
+                }
+                 Console.Read();
+        }       
+    }
+}
