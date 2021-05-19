2a4d4c452005f835522e14f6581265de46b11efa
onurakyildiz58 <56562773+onurakyildiz58@users.noreply.github.com>
2021-05-19 22:50:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _191307026_OnurAkyıldız_Ödev9.txt
+{
+    public class Base
+    {
+        public virtual void Test()
+        {
+            Console.WriteLine("Bu, sanal metotun temel sürümüdür");
+        }
+    }
+    public class Derived : Base
+    {
+        public override void Test()
+        {
+            Console.WriteLine("Bu, sanal metotun türetilmiş sürümüdür");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Base base1 = new Base();
+            base1.Test();
+            Base base2 = new Derived();
+            base2.Test();
+
+
+            Console.ReadKey();
+        }
+    }
+}
